2081bd7ff024624618f30ba20aa4ccf66064a92f
vishal <vishalborkar9072@gmail.com>
2021-12-28 06:15:56 -0800

    first commit

---
+++ src/test/java/qaclickacademy/seleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+	@Test
+	public void Browserautomation()
+	{
+		System.out.println("Browserautomation");
+	}
+	
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUi");
+	}
+}
